67872771054472c79afb4a83fb3862cb0f39eb42
W-quan <863102331@qq.com>
2022-06-17 16:29:55 +0800

    Update AndroidHardedStringExtractor.py

--- AndroidHardedStringExtractor.py
+++ AndroidHardedStringExtractor.py
@@ -2,15 +2,21 @@ import os,re, hashlib
 
 '''
 Android Hardcoded String Extractor
-配合rigrep 或者 ack 搜索出需要extract的地方
 
-explamples:
+1. 配合`rigrep` 或者 `ack` 搜索出需要extract的地方
 
-搜索代码中的中文string:    rg '".*[\u4e00-\u9fa5].*"' -tjava -tkotlin --line-number > text.txt
+    explamples:
 
-搜索layout文件中的string:   rg 'android:text=' > text.txt
+    搜索代码中的中文string:    `rg '".*[\u4e00-\u9fa5].*"' -tjava -tkotlin --line-number > grep_file.txt`
 
-手动检查删除不需要的行，暂时不能精确的搜索出来
+    搜索layout文件中的string:   `rg 'android:text=' > grep_file.txt`
+
+2. 手动检查删除不需要的行，暂时不能精确的搜索出来
+
+3. `python3 AndroidHardedcodeStringExtractor.py`
+
+4. 剩下靠勤劳的双手
+
 '''
 
 # rg/ack 搜索生成的文件
